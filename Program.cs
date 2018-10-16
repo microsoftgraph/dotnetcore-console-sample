c12cc70c6d61b4946c419df132e8208d1ee17132
Brian T. Jackett <BrianTJackett@outlook.com>
2018-10-16 09:38:15 -0400

    Static variables for clients.  Move authenticated clients into methods.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using Microsoft.Identity.Client;
-using Microsoft.Graph;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.FileExtensions;
-using Microsoft.Extensions.Configuration.Json;
 
 namespace ConsoleGraphTest
 {
     class Program
     {
+        private static GraphServiceClient _graphServiceClient;
+        private static HttpClient _httpClient;
+
         static void Main(string[] args)
         {
             // Load appsettings.json
@@ -23,19 +24,38 @@ namespace ConsoleGraphTest
                 return;
             }
 
+            //Query using Graph SDK (preferred when possible)
+            GraphServiceClient graphClient = GetAuthenticatedGraphClient(config);
+            List<QueryOption> options = new List<QueryOption>
+            {
+                new QueryOption("$top", "1")
+            };
+
+            var graphResult = graphClient.Users.Request(options).GetAsync().Result;
+            Console.WriteLine(graphResult);
+
+            //Direct query using HTTPClient (for beta endpoint calls or not available in Graph SDK)
+            HttpClient httpClient = GetAuthenticatedHTTPClient(config);
+            Uri Uri = new Uri("https://graph.microsoft.com/v1.0/users?$top=1");
+            var httpResult = httpClient.GetStringAsync(Uri).Result;
+
+            Console.WriteLine(httpResult);
+        }
+
+        private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
+        {
             var clientId = config["applicationId"];
             var clientSecret = config["applicationSecret"];
-            var redirectUri = "https://localhost:8042";
+            var redirectUri = config["redirectUri"];
             var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
             List<string> scopes = new List<string>();
             scopes.Add("https://graph.microsoft.com/.default");
 
             var cca = new ConfidentialClientApplication(clientId, authority, redirectUri, new ClientCredential(clientSecret), null, null);
             var authResult = cca.AcquireTokenForClientAsync(scopes).Result;
 
-
-            //Query using Graph SDK (preferred when possible)
-            GraphServiceClient graphServiceClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) =>
+            _graphServiceClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) =>
             {
                 requestMessage
                     .Headers
@@ -44,24 +64,33 @@ namespace ConsoleGraphTest
                 return Task.FromResult(0);
             }));
 
-            List<QueryOption> options = new List<QueryOption>
-            {
-                new QueryOption("$top", "1")
-            };
-
-            var graphResult = graphServiceClient.Users.Request(options).GetAsync().Result;
-            Console.WriteLine(graphResult);
-
-
-            //Direct query using HTTPClient (for beta endpoint calls or not available in Graph SDK)
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization", "bearer " + authResult.AccessToken);
-
-            Uri Uri = new Uri("https://graph.microsoft.com/v1.0/users?$top=1");
-            var httpResult = client.GetStringAsync(Uri).Result;
-
-            Console.WriteLine(httpResult);
+            return _graphServiceClient;
         }
+
+        private static HttpClient GetAuthenticatedHTTPClient(IConfigurationRoot config)
+        {
+            var clientId = config["applicationId"];
+            var clientSecret = config["applicationSecret"];
+            var redirectUri = config["redirectUri"];
+            var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+            List<string> scopes = new List<string>();
+            scopes.Add("https://graph.microsoft.com/.default");
+
+            var cca = new ConfidentialClientApplication(clientId, authority, redirectUri, new ClientCredential(clientSecret), null, null);
+            var authResult = cca.AcquireTokenForClientAsync(scopes).Result;
+
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "bearer " + authResult.AccessToken);
+
+            return _httpClient;
+        }
+
+        // private static string GetAccessToken()
+        // {
+            
+
+        // }
 
         private static IConfigurationRoot LoadAppSettings()
         {
@@ -75,6 +104,7 @@ namespace ConsoleGraphTest
                 // Validate required settings
                 if (string.IsNullOrEmpty(config["applicationId"]) ||
                     string.IsNullOrEmpty(config["applicationSecret"]) ||
+                    string.IsNullOrEmpty(config["redirectUri"]) ||
                     string.IsNullOrEmpty(config["tenantId"]))
                 {
                     return null;
