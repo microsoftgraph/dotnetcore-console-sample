96a85460f222627b2e466a2eba8cf4484a9edc5b
Brian T. Jackett <BrianTJackett@users.noreply.github.com>
2018-11-06 20:25:55 -0500

    Fix typo for "concerns"

--- Helpers/AuthHandler.cs
+++ Helpers/AuthHandler.cs
@@ -12,7 +12,7 @@ using System.Threading;
 namespace ConsoleGraphTest
 {
     // This class allows an implementation of IAuthenticationProvider to be inserted into the DelegatingHandler
-    // pipeline of an HttpClient instance.  In future versions of GraphSDK, many cross-cutting concernts will
+    // pipeline of an HttpClient instance.  In future versions of GraphSDK, many cross-cutting concerns will
     // be implemented as DelegatingHandlers.  This AuthHandler will come in the box.
     public class AuthHandler : DelegatingHandler {
         private IAuthenticationProvider _authenticationProvider;
