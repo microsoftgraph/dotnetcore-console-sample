3ca6a545c79c9b0816208ca09a46868b8c9e4b22
Vincent Biret <vincentbiret@hotmail.com>
2018-11-12 21:32:08 -0500

    first implementation of planner requests

--- dayNN-template/Program.cs
+++ dayNN-template/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 using Microsoft.Graph;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace ConsoleGraphTest
 {
@@ -23,41 +24,88 @@ namespace ConsoleGraphTest
                 Console.WriteLine("Missing or invalid appsettings.json file. Please see README.md for configuration instructions.");
                 return;
             }
-
+            PlannerHelperCall(config).GetAwaiter().GetResult();
+        }
+        private static async Task PlannerHelperCall(IConfigurationRoot config) {
             //Query using Graph SDK (preferred when possible)
-            GraphServiceClient graphClient = GetAuthenticatedGraphClient(config);
-            List<QueryOption> options = new List<QueryOption>
-            {
+            var graphClient = GetAuthenticatedGraphClient(config);
+
+            await ListCurrentPlans(graphClient);
+
+            //Getting the first group we can find to create a plan
+            var group = (await graphClient.Groups.Request(new List<QueryOption> {
                 new QueryOption("$top", "1")
-            };
+            }).GetAsync()).FirstOrDefault();
 
-            var graphResult = graphClient.Users.Request(options).GetAsync().Result;
-            Console.WriteLine("Graph SDK Result");
-            Console.WriteLine(graphResult[0].DisplayName);
-
-            //Direct query using HTTPClient (for beta endpoint calls or not available in Graph SDK)
-            HttpClient httpClient = GetAuthenticatedHTTPClient(config);
-            Uri Uri = new Uri("https://graph.microsoft.com/v1.0/users?$top=1");
-            var httpResult = httpClient.GetStringAsync(Uri).Result;
-
-            Console.WriteLine("HTTP Result");
-            Console.WriteLine(httpResult);
-     
-            // Call your method wrapping construction and calls to the helper
-            MyHelperCall();
+            if(group != null) {
+                var users = await graphClient.Users.Request(new List<QueryOption> {
+                        new QueryOption("$top", "3")
+                    }).GetAsync();
+                
+                var planId = await CreatePlannerPlan(graphClient, users, group.Id);
+                var bucketId = await CreatePlannerBucket(graphClient, planId);
+                await CreatePlannerTask(graphClient, users, planId, bucketId);
+            }
         }
-
-        // Add a private method to do any necessary setup and make calls to your helper
-        private static void MyHelperCall()
-        {
-            const string alias = "sdk_test";
-            var userHelper = new MyHelper(_graphServiceClient);
-            var user = userHelper.FindByAlias(alias).Result;
-            // Add some console writes for demo purposes if necessary
-            Console.WriteLine(user.DisplayName);
-            Console.WriteLine(user.UserPrincipalName);       
+        private static async Task ListCurrentPlans(GraphServiceClient graphClient) {
+            //Querying plans in current tenant
+            var plans = await graphClient.Planner.Plans.Request(new List<QueryOption>
+            {
+                new QueryOption("$orderby", "Title asc")
+            }).GetAsync();
+            Console.WriteLine($"Number of plans in current tenant: {plans.Count}");
+            Console.Write(plans.Select(x => $"-- {x.Title}").Aggregate((x,y) => $"{x}\n{y}"));
         }
+        private static async Task<string> CreatePlannerPlan(GraphServiceClient graphClient, IEnumerable<User> users, string groupId) {
+            // Getting users to share the plan with
+            var sharedWith = new PlannerUserIds();
+            users.ToList().ForEach(x => sharedWith.Add(x.Id));
 
+            // Creating a new planner plan
+            var createdPlan = await graphClient.Planner.Plans.Request().AddAsync(
+                new PlannerPlan {
+                    Title = $"My new Plan {Guid.NewGuid().ToString()}",
+                    Owner = groupId,
+                    Details = new PlannerPlanDetails {
+                        SharedWith = sharedWith,
+                        CategoryDescriptions = new PlannerCategoryDescriptions {
+                            Category1 = "my first category",
+                            Category2 = "my second category"
+                        },
+                    }
+                }
+            );
+            Console.WriteLine($"Added a new plan {createdPlan.Id}");
+            return createdPlan.Id;
+        }
+        private static async Task<string> CreatePlannerBucket(GraphServiceClient graphClient, string planId) {
+            // Creating a new bucket within the plan
+            var createdBucket = await graphClient.Planner.Plans[planId].Buckets.Request().AddAsync(
+                new PlannerBucket {
+                    Name = "my first bucket",
+                    OrderHint = 1.ToString()
+                }
+            );
+            Console.WriteLine($"Added new bucket {createdBucket.Name} to plan");
+            return createdBucket.Id;
+        }
+        private static async Task CreatePlannerTask(GraphServiceClient graphClient, IEnumerable<User> users, string planId, string bucketId){
+            // Preparing the assignment for the task
+            var assignments = new PlannerAssignments ();
+            users.ToList().ForEach( x=> assignments.AddAssignee(x.Id));
+            // Creating a task within the bucket
+            var createdTask = await graphClient.Planner.Plans[planId].Buckets[bucketId].Tasks.Request().AddAsync(
+                new PlannerTask {
+                    DueDateTime = DateTimeOffset.UtcNow.AddDays(7),
+                    Title = "Do the dishes",
+                    Details = new PlannerTaskDetails {
+                        Description = "Do the dishes that are remaining in the sink"
+                    },
+                    Assignments = assignments
+                }
+            );
+            Console.WriteLine($"Added new task {createdTask.Title} to bucket");
+        }
         private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
         {
             var authenticationProvider = CreateAuthorizationProvider(config);
