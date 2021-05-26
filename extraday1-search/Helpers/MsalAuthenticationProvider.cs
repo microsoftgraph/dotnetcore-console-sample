5bd356323c489132176636c1371ea91d99fe4349
Yiwen Wang (MSAI) <yiwenwang@microsoft.com>
2021-05-26 16:39:06 +0800

    revert old codes

---
+++ extraday1-search/Helpers/MsalAuthenticationProvider.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace ConsoleGraphTest
+{
+    // This class encapsulates the details of getting a token from MSAL and exposes it via the 
+    // IAuthenticationProvider interface so that GraphServiceClient or AuthHandler can use it.
+    // A significantly enhanced version of this class will in the future be available from
+    // the GraphSDK team.  It will supports all the types of Client Application as defined by MSAL.
+    public class MsalAuthenticationProvider : IAuthenticationProvider
+    {
+        private IConfidentialClientApplication _clientApplication;
+        private string[] _scopes;
+
+        public MsalAuthenticationProvider(IConfidentialClientApplication clientApplication, string[] scopes) {
+            _clientApplication = clientApplication;
+            _scopes = scopes;
+        }
+
+        /// <summary>
+        /// Update HttpRequestMessage with credentials
+        /// </summary>
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            var token = await GetTokenAsync();
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+        }
+
+        /// <summary>
+        /// Acquire Token 
+        /// </summary>
+        public async Task<string> GetTokenAsync()
+        {
+            AuthenticationResult authResult = null;
+            authResult = await _clientApplication.AcquireTokenForClient(_scopes)
+                                .ExecuteAsync();
+            return authResult.AccessToken;
+        }
+    }
+}
